2321d123ab5f851247af0a5e2fdff4bbcd4fbf02
kason-seu <zhangkaiseu@163.com>
2024-04-20 13:19:22 +0800

    添加涨跌幅

--- autobn_realtime/mystrategy/k15m_strategy.py
+++ autobn_realtime/mystrategy/k15m_strategy.py
@@ -30,37 +30,14 @@ class K15Strategy:
             f"coin={coin}, real_time={event_time}, open_price={open_price}, close_price={close_price}, "
             f"upper_ratio={upper_ratio_percentage}, lower_ratio={lower_ratio_percentage}")
 
-        if (open_price > close_price) and (lower_ratio < -0.05):
-            quantity = bnutils.get_quantity(close_price, 10)
-            logger.info(
-                f"触发了下界条件是lower_ratio < -0.05,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
-                f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, quantity={quantity}")
-            print(f"触发了下界条件是lower_ratio < -0.05,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
-                f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, quantity={quantity}")
-            bn_op.buy_crypto_by_limit(client=self.client, symbol=coin, quantity=quantity, price=close_price)
-            send_dingtalk_message(
-                message=f"触发了下界条件是lower_ratio < -0.05,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
-                        f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, 买的quantity={quantity}")
-
-        if (open_price < close_price) and (upper_ratio > 0.05):
-            logger.info(
-                f"触发了上界条件是upper_ratio > 0.05,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
-                f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
-            print(f"触发了上界条件是upper_ratio > 0.05,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
-                f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
-            bn_op.sell_crypto_by_usdt_limit(client=self.client, symbol=coin, usdt_amount=10, sell_price=close_price)
-            send_dingtalk_message(
-                message=f"触发了上界条件是upper_ratio > 0.05,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
-                f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
-
         if (open_price > close_price) and (lower_ratio < -0.1):
             quantity = bnutils.get_quantity(close_price, 10)
             logger.info(
                 f"触发了下界条件是lower_ratio < -0.1,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
                 f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, quantity={quantity}")
-            print(f"触发了下界条件是lower_ratio < -0.1,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
-                f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, quantity={quantity}")
-            bn_op.buy_crypto_by_limit(client=self.client, symbol=coin, quantity=quantity, price=close_price)
+            # print(f"触发了下界条件是lower_ratio < -0.1,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
+            #     f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, quantity={quantity}")
+            #bn_op.buy_crypto_by_limit(client=self.client, symbol=coin, quantity=quantity, price=close_price)
             send_dingtalk_message(
                 message=f"触发了下界条件是lower_ratio < -0.1,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
                         f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, 买的quantity={quantity}")
@@ -69,9 +46,49 @@ class K15Strategy:
             logger.info(
                 f"触发了上界条件是upper_ratio > 0.1,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
                 f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
-            print(f"触发了上界条件是upper_ratio > 0.1,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
-                f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
-            bn_op.sell_crypto_by_usdt_limit(client=self.client, symbol=coin, usdt_amount=10, sell_price=close_price)
+            # print(f"触发了上界条件是upper_ratio > 0.05,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
+            #     f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
+            #bn_op.sell_crypto_by_usdt_limit(client=self.client, symbol=coin, usdt_amount=10, sell_price=close_price)
             send_dingtalk_message(
                 message=f"触发了上界条件是upper_ratio > 0.1,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
+                f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
+
+        if (open_price > close_price) and (lower_ratio < -0.2):
+            quantity = bnutils.get_quantity(close_price, 10)
+            logger.info(
+                f"触发了下界条件是lower_ratio < -0.2,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
+                f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, quantity={quantity}")
+            # print(f"触发了下界条件是lower_ratio < -0.1,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
+            #     f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, quantity={quantity}")
+            # bn_op.buy_crypto_by_limit(client=self.client, symbol=coin, quantity=quantity, price=close_price)
+            send_dingtalk_message(
+                message=f"触发了下界条件是lower_ratio < -0.2,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
+                        f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, 买的quantity={quantity}")
+
+        if (open_price < close_price) and (upper_ratio > 0.2):
+            logger.info(
+                f"触发了上界条件是upper_ratio > 0.2,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
+                f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
+            # print(f"触发了上界条件是upper_ratio > 0.1,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
+            #     f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
+            # bn_op.sell_crypto_by_usdt_limit(client=self.client, symbol=coin, usdt_amount=10, sell_price=close_price)
+            send_dingtalk_message(
+                message=f"触发了上界条件是upper_ratio > 0.2,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
+                f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
+
+        if (open_price > close_price) and (lower_ratio < -0.15):
+            quantity = bnutils.get_quantity(close_price, 10)
+            logger.info(
+                f"触发了下界条件是lower_ratio < -0.2,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
+                f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, quantity={quantity}")
+            send_dingtalk_message(
+                message=f"触发了下界条件是lower_ratio < -0.15,这是向下插强针了,启动购买操作,coin={coin}, real_time={event_time}, "
+                        f"lower_ratio={lower_ratio_percentage}, open_price={open_price}, close_price={close_price}, 买的quantity={quantity}")
+
+        if (open_price < close_price) and (upper_ratio > 0.15):
+            logger.info(
+                f"触发了上界条件是upper_ratio > 0.2,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
+                f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")
+            send_dingtalk_message(
+                message=f"触发了上界条件是upper_ratio > 0.2,这是向上狂拉了,启动售卖操作,coin={coin}, real_time={event_time}, "
                 f"upper_ratio={upper_ratio_percentage}, open_price={open_price}, close_price={close_price}")

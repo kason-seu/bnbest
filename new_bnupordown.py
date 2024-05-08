57e473e2b2d3be024237a6413c83916bb6ee1169
kason-seu <zhangkaiseu@163.com>
2024-05-08 23:26:18 +0800

    更改K线逻辑与24小时涨跌幅逻辑。 增加限流器不阻塞主线程，控制5分钟内的预警情况

--- new_bnupordown.py
+++ new_bnupordown.py
@@ -104,11 +104,11 @@ def on_message(ws, message):
             alert_message = f"下跌警告下跌超过20%！{coin} 24小时涨跌幅已达 {price_change_percent}%"
             logger.info(alert_message)
             add_to_bound_queue(alert_message)  # 将消息放入队列
-        elif price_change_percent <= -13:
-            alert_message = f"下跌警告下跌超过13%！{coin} 24小时涨跌幅已达 {price_change_percent}%"
+        elif price_change_percent <= -17:
+            alert_message = f"下跌警告下跌超过15%！{coin} 24小时涨跌幅已达 {price_change_percent}%"
             logger.info(alert_message)
             add_to_bound_queue(alert_message)  # 将消息放入队列
-        elif price_change_percent <= -10:
+        elif price_change_percent <= -15:
             alert_message = f"下跌警告下跌超过10%！{coin} 24小时涨跌幅已达 {price_change_percent}%"
             logger.info(alert_message)
             add_to_bound_queue(alert_message)  # 将消息放入队列
